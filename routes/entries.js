0f62d8f2a48499b63f051ad7bcbfaac7765fdf2f
AyshaChowdhury <144631718+AyshaChowdhury@users.noreply.github.com>
2025-11-23 05:30:59 -0500

    Added entry form addentry.ejs and modified routes.

--- routes/entries.js
+++ routes/entries.js
@@ -4,8 +4,8 @@
 
     // Load the PetCareEntry model (models/entrymodel.js)
     let PetCareEntry = require('../models/entrymodel');
-
-    // Read all pet care journal entries
+    
+    // GET route for displaying all journal entries - Read Operation
     router.get('/', async (req, res, next) => {
     try {
         const entries = await PetCareEntry.find().sort({ date: -1 });
@@ -16,6 +16,54 @@
     } catch (err) {
         next(err);
     }
+    });
+
+    //GET route for displaying the Add Entry page - Create Operation
+    router.get('/addentry', async (req, res, next) => {
+        try {
+            res.render('addentry', {
+                title: 'Add New Entry'
+            });
+        } catch (err) {
+            next(err);
+        }
+    });
+
+    //POST route for processing the Add Entry form - Create Operation
+    router.post('/addentry', async (req, res, next) => {
+        try {
+            let newEntry = new PetCareEntry({
+                "petName": req.body.petName,
+                "petType": req.body.petType,
+                "age": req.body.age,
+                "entryType": req.body.entryType,
+                "date": req.body.date,
+                "vetName": req.body.vetName,
+                "nextAppointment": req.body.nextAppointment,
+                "notes": req.body.notes
+            });
+
+            PetCareEntry.create(newEntry).then(() => {
+                res.redirect('/entries');
+            })
+        } catch (err) {
+            next(err);
+        }   
+    });
+
+    //GET route for displaying the Edit Entry page - Update Operation
+    router.get('/edit/:id', async (req, res, next) => {
+
+    });
+
+    //POST route for processing the Edit Entry form - Update Operation
+    router.post('/edit/:id', async (req, res, next) => {
+
+    });
+
+    //GET route for deleting an entry - Delete Operation
+    router.get('/delete/:id', async (req, res, next) => {   
+
     });
 
     // Export the router so app.js can use it
